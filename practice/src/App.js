6aa69fe6c9cf0fb0ba7ef0b270c8ff1367bdcaee
Muhammad Saad <msaad.bscs22seecs@seecs.edu.pk>
2023-07-06 15:43:52 +0500

    Done

--- practice/src/App.js
+++ practice/src/App.js
@@ -3,13 +3,11 @@ import './App.css';
 import { useSelector,useDispatch } from 'react-redux';
 import Test from './Test';
 import React,{useState} from 'react';
-import Admin from './Admin';
 function App() {
   const v = useSelector(state=>state.value); //useSelector for getting values of the state 
 
   return (
 <>
-    <Admin/>
     </>
   );
 }
--- practice/src/index.js
+++ practice/src/index.js
@@ -1,24 +1,11 @@
 //index.js
-
+//index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import { createStore, applyMiddleware, combineReducers } from 'redux';
-// import logger from 'redux-logger';
-// import reducer from "./redux/reducers";
-import { Provider } from 'react-redux';
-// const store = createStore(reducer, applyMiddleware(logger));
-import accountReducer from "./slices/accountSlice";
-import { adminApi } from './api/adminSlice';
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 
-const store = configureStore({
-  reducer:{account:accountReducer,[adminApi.reducerPath]:adminApi.reducer},
-  middleware:(getDefaultMiddleware) => getDefaultMiddleware().concat(adminApi.middleware),
-  
-})
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
